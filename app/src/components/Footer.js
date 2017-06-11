db48816ff39580f76aa49130e2f93edb7aaa07ed
Nick McKoy <nmckoy10@gmail.com>
2017-06-11 21:58:53 +0000

    switch up homepage content layout

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { ListGroup, ListGroupItem, ButtonGroup, Button } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const text_content_sub = { 
   minHeight: '100px',
@@ -9,10 +10,9 @@ const text_content_sub = {
   padding: '30px 80px',
   // https://uigradients.com/#Passion
   textAlign: 'justify',
-  background: '#e53935',  /* fallback for old browsers */
-  background: '-webkit-linear-gradient(to right, #e35d5b, #e53935)',  /* Chrome 10-25, Safari 5.1-6 */
-  background: 'linear-gradient(to right, #e35d5b, #e53935)' /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+  background: '#ECE9E6',  /* fallback for old browsers */
+  background: '-webkit-linear-gradient(to right, #FFFFFF, #ECE9E6)',  /* Chrome 10-25, Safari 5.1-6 */
+  background: 'linear-gradient(to right, #FFFFFF, #ECE9E6)' /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 const text_content_sub_pos = {
   position:'relative',
@@ -26,38 +26,26 @@ class Footer extends Component {
     return (
         <div style={ text_content_sub_pos }>
           	<div style={ text_content_sub }>
-            	  <div className='container'>
-                  {/*<div className='col-md-4'>
-                    <ListGroup className="footer-group-item-style">
-                    ` <ListGroupItem ><a id="footer-links-title" href='#'>Links</a></ListGroupItem>
-                      <ListGroupItem ><Link to='/'>Home</Link></ListGroupItem>
-                      <ListGroupItem ><Link to='/about'>About Us</Link></ListGroupItem>
-                      <ListGroupItem ><Link to='/portfolio'>Portfolio</Link></ListGroupItem>
-                      <ListGroupItem ><Link to='/events'>Events</Link></ListGroupItem>
-                      <ListGroupItem ><Link to='/contact'>Contact Us</Link></ListGroupItem>
-                    </ListGroup>
-                  </div> */}
-                  <div className='row'>
+              <Grid>
+                <Row>
+                  <Col xs>
                     <div className='col-md-12 footer-logo'>
                       {/* <img src='/assets/jacy_black_transparent_trimmed.png' width='300' height='150'/> */}
                     </div>
-                  </div>
-                  <div className='row'>
-                    <div className='col-md-12'>
-                      <div className='col-md-4'>
-                      </div>
-                      <div className='col-md-4'>
-                        <div className='footer-social-buttons'>
-                          <Button ><a href="https://www.facebook.com/jrhamz"><i className="fa fa-facebook" aria-hidden="true"></i></a></Button>
-                          <Button ><a href="mailto:j@thejacymethod.com"><i className="fa fa-envelope" aria-hidden="true"></i></a></Button>
-                          <Button ><a href="https://www.instagram.com/jacycunningham/"><i className="fa fa-instagram" aria-hidden="true"></i></a></Button>
-                        </div>
-                      </div>
-                      <div className='col-md-4'>
-                      </div>
-                    </div>
-                  </div>
-            	  </div>
+                  </Col>
+                </Row>
+                  <Col xsOffset={6} xs className='footer-social-buttons'>
+                    <Row>
+                      <Col xsOffset={4} xs>
+                        <Button ><a href="https://www.facebook.com/jrhamz"><i className="fa fa-facebook" aria-hidden="true"></i></a></Button>
+                        <Button ><a href="mailto:j@thejacymethod.com"><i className="fa fa-envelope" aria-hidden="true"></i></a></Button>
+                        <Button ><a href="https://www.instagram.com/jacycunningham/"><i className="fa fa-instagram" aria-hidden="true"></i></a></Button>
+                      </Col>
+                    </Row>
+                  </Col>
+                <Row>
+                </Row>
+              </Grid>
           	</div>
         </div>
     )}
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { Button } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import EventListener, { withOptions } from 'react-event-listener'
 
 import Footer from '../components/Footer'
+
 // https://facebook.github.io/react/docs/dom-elements.html#style
-const text_content_lead = {minHeight: '250px', color: '#777', backgroundColor: 'white', textAlign: 'center', padding: '25px', textAlign: 'justify'}
+const text_content_lead = {
+  minHeight: '1000px',
+  color: '#777',
+  backgroundColor: 'white',
+  textAlign: 'center',
+  padding: '25px',
+  textAlign: 'justify'
+}
 const text_content_lead_header = {textAlign: 'center'}
 
 const bgimg_sub_button = { backgroundColor: 'transparent', fontSize: '25px', color: '#f7f7f7' }
 
-const text_content_sub = { minHeight: '500px', color: 'rgb(35, 35, 35)', backgroundColor: 'rgb(243, 243, 243)', textAlign: 'center', padding: '50px 80px', textAlign: 'justify' }
-const text_content_sub_pos = { position:'relative' }
+const text_content_sub = {
+  minHeight: '500px',
+  color: 'rgb(35, 35, 35)',
+  backgroundColor: 'rgb(243, 243, 243)',
+  textAlign: 'center',
+  padding: '50px 80px',
+  textAlign: 'justify'
+}
 
 const bgimg_first_logo_style = { padding: '15px' }
 const bgimg_first_button_style = { padding: '30px' }
+const text_content_sub_pos = { position:'relative' }
 
 class Home extends Component {
+  showBoxes() {
+    let scrollTop = event.srcElement.body.scrollTop // so i guess .srcElement == target
+    console.log(scrollTop)
+    if (scrollTop > 420) {
+      document.getElementById('home-row1-box1').setAttribute('style', 'right: 0px; position: relative;')
+    }
+    if (scrollTop > 450) {
+      document.getElementById('home-row1-box2').setAttribute('style', 'left: 0px; position: relative;')
+    }
+    
+    if (scrollTop > 620) {
+      document.getElementById('home-row2-box1').setAttribute('style', 'right: 0px; position: relative;')
+    }
+    if (scrollTop > 650) {
+      document.getElementById('home-row2-box2').setAttribute('style', 'left: 0px; position: relative;')
+    }
+    
+    if (scrollTop > 820) {
+      document.getElementById('home-row3-box1').setAttribute('style', 'right: 0px; position: relative;')
+    }
+    if (scrollTop > 850) {
+      document.getElementById('home-row3-box2').setAttribute('style', 'left: 0px; position: relative;')
+    }
+  }
+  
   render() {
     return(
       <div>
+        <EventListener target="window" onScroll={ this.showBoxes }/>
+        
         <div className="home-bgimg-1">
           {/*jacy_white_transparent_cropped*/}
           {/* <div className="caption">
@@ -32,47 +76,52 @@ class Home extends Component {
           
         <div style={ text_content_lead} className='container'>
             <div className='col-md-12'>
-              <div className='col-md-4 home-sub-box-container'>
-                <Link to='/portfolio'>
-                  <div className='home-sub-box1'>
-                    <div className='home-sub-link-banner'>
-                      Portfolio
+              <Grid >
+                <Row className='home-rows'> {/* Row 1 */}
+                  <Col xsOffset={1} xs={5} className='home-row-box-wrapper'>
+                    <div id='home-row1-box1' className='home-box'>
                     </div>
-                  </div>
-                </Link>
-              </div>
-              <div className='col-md-4 home-sub-box-container'>
-                <Link to='/events'>
-                  <div className='home-sub-box2'>
-                    <div className='home-sub-link-banner'>
-                      Past Events
+                  </Col>
+                  <Col xs={5} className='home-row-box-wrapper'>
+                    <div id='home-row1-box2' className='home-box'>
+                      <h2>header1</h2>
+                      <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</p>
                     </div>
-                  </div>
-                </Link>
-              </div>
-              <div className='col-md-4 home-sub-box-container'>
-                <Link to='/about'>
-                  <div className='home-sub-box3'>
-                    <div className='home-sub-link-banner'>
-                      Learn More
+                  </Col>
+                </Row>
+                <Row className='home-rows'> {/* Row 2 */}
+                  <Col xsOffset={2} xs={5} className='home-row-box-wrapper'>
+                    <div id='home-row2-box1' className='home-box'>
                     </div>
-                  </div>
-                </Link>
-              </div>
+                  </Col>
+                  <Col xs={5} className='home-row-box-wrapper'>
+                    <div id='home-row2-box2' className='home-box'>
+                      <h2>header2</h2>
+                      <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</p>
+                    </div>
+                  </Col>
+                </Row>
+                <Row className='home-rows'> {/* Row 3 */}
+                  <Col xs={5} className='home-row-box-wrapper'>
+                    <div id='home-row3-box1' className='home-box'>
+                    </div>
+                  </Col>
+                  <Col xs={5} className='home-row-box-wrapper'>
+                    <div id='home-row3-box2' className='home-box'>
+                      <h2>header3</h2>
+                      <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children.</p>
+                    </div>
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
+              </Grid>
             </div>
         </div>
         
-        <div style={ text_content_sub }>
+        {/* <div style={ text_content_sub }>
             <div className='col-md-12'>
-              <div className='col-md-12 home-sub-box-container'>
-                 <video height="400px" id="jacy-media-player" controls='controls' preload="auto">
-                    <source type="video/mp4" src="/assets/jacy_in_action.mp4" />
-                </video>
-                {/* <div className='home-vid-box1'>
-                </div>*/}
-              </div>
             </div>
-        </div>
+        </div> */}
           
         {/*<div className="home-bgimg-2">
           	<div>
